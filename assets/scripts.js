5930c58b28b11da646a55313776a08f6a70f7d46
Gerard <gkhynes@gmail.com>
2017-11-01 21:25:09 +0000

    Initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,19 @@
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
+
+function handleEnter() {
+  this.classList.add("trigger-enter");
+  setTimeout(() => this.classList.add("trigger-enter-active"), 150);
+}
+
+function handleLeave() {
+  console.log("LEAVE!");
+}
+
+triggers.forEach(trigger =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+triggers.forEach(trigger =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);
